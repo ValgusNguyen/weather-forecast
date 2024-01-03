23d9a624d52a322d2711e05bfcf1e00c8e7e0b5f
Valgus <khuenguyenhaminh.com>
2024-01-04 00:56:56 +0700

    fix: openWeather api

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,28 +6,21 @@ import './LeftSide.css';
 const Search = ({ onSearchChange }: any) => {
 	const [search, setSearch] = useState();
 	const loadOptions = async (inputValue: any) => {
-		try {
-			// TODO: use new URL to build out the url before calling
-			const url = `${GEO_URL}/cities?namePrefix=${inputValue}`;
-			// const url = `${GEO_URL}/cities?namePrefix="Hanoi"`;
+		// TODO: use new URL to build out the url before calling
+		const url = `${GEO_URL}/cities?namePrefix=${inputValue}`;
 
-			// TODO: use axios for best practices
-			const response = await fetch(url, geoApiOptions);
-			const result = await response.json();
+		// TODO: use axios for best practices
+		const response = await fetch(url, geoApiOptions);
+		const result = await response.json();
 
-			// console.log(result);
-			return {
-				options: result.data.map((result: Record<string, string>) => ({
-					value: `${result.latitude} ${result.longitude}`,
-					label: `${result.name}`,
-				})),
-				hasMore: true,
-			};
-			// console.log(location,"loc")
-		} catch (error) {
-			// TODO: find a way to handle error in api call failed
-			console.error(error);
-		}
+		// console.log(result);
+		return {
+			options: result.data.map((result: Record<string, string>) => ({
+				value: `${result.latitude} ${result.longitude}`,
+				label: `${result.name}`,
+			})),
+			hasMore: true,
+		};
 	};
 
 	const handleOnChange = (searchData: any) => {
