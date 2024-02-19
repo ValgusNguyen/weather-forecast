89d4bcd3fd32bfff7595939f67a8aa7460e52eda
AustinHuynh248 <huynhtransongngoc@gmail.com>
2024-02-19 19:30:14 +0700

    fix: windowWidth initial value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,25 @@
 import { Location } from '@/components/Location';
 import WeatherInfo from '@/components/WeatherInfo';
 import styles from '@/styles/page.module.css';
-import { getUserWeather } from '@/utils/geoLocation';
 import { ChangeEvent, useEffect, useState } from 'react';
 
 export default function Page() {
 	const [isFlipped, setIsFlipped] = useState(false);
-	const [windowWidth, setWindowWidth] = useState(0);
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
 	useEffect(() => {
-		if (typeof window !== 'undefined') {
-			const handleResize = () => {
-				setWindowWidth(window.innerWidth);
-			};
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
 
-			window.addEventListener('resize', handleResize);
+		window.addEventListener('resize', handleResize);
 
-			return () => {
-				window.removeEventListener('resize', handleResize);
-			};
-		}
-	}, []);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	});
 
+	console.log(windowWidth);
 	return (
 		<div className={styles['card-container']}>
 			<Location
